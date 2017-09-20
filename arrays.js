9bc1eced3b5939a328405abac1822c1ce565c842
Domnik Mack <domnikmack@gmail.com>
2017-09-20 17:37:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,1 @@
 var chocolateBars = ['sickers', 'hundred grand', 'kitkat', 'skittles'];
-
-function addElementToBeginningOfArray (array, element) {
-  return [element, ...array];
-}
-
-  function destructivelyAddElementToBeginningOfArray (array, element) {
-    array.unshift (element);
-  }
-
-  function addElementToEndOfArray (array, element) {
-    array = ;
-  }
-
-  function destructivelyAddElementToEndOfArray (array, element) {
-    array.push (element);
-}
